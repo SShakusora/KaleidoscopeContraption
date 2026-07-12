a7776537e8f2f1f982897882b65be3d65026e4e1
笏天 <1404928002@qq.com>
2026-07-12 10:22:52 +0800

    fix: add chopping board removal. https://github.com/SShakusora/KaleidoscopeContraption/issues/3

--- src/main/java/com/sshakusora/kaleidoscope_contraption/content/behaviour/placement/TableChoppingBoardPlacementRule.java
+++ src/main/java/com/sshakusora/kaleidoscope_contraption/content/behaviour/placement/TableChoppingBoardPlacementRule.java
@@ -1,14 +1,14 @@
 package com.sshakusora.kaleidoscope_contraption.content.behaviour.placement;
 
 import com.github.ysbbbbbb.kaleidoscopecookery.block.kitchen.ChoppingBoardBlock;
-import com.sshakusora.kaleidoscope_contraption.api.placement.ContraptionPlacementContext;
-import com.sshakusora.kaleidoscope_contraption.api.placement.ContraptionPlacementResult;
-import com.sshakusora.kaleidoscope_contraption.api.placement.ContraptionPlacementRule;
+import com.sshakusora.kaleidoscope_contraption.api.placement.*;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import java.util.Optional;
 
 public class TableChoppingBoardPlacementRule implements ContraptionPlacementRule {
     @Override
@@ -30,5 +30,23 @@ public class TableChoppingBoardPlacementRule implements ContraptionPlacementRule
         nbt.putString("id", "kaleidoscope_cookery:chopping_board");
         return ContraptionPlacementResult.single(new StructureTemplate.StructureBlockInfo(
                 context.targetPos(), state, nbt));
+    }
+
+    @Override
+    public Optional<ContraptionRemovalResult> createRemoval(ContraptionRemovalContext context) {
+        BlockState state = context.targetInfo().state();
+        if (!(state.getBlock() instanceof ChoppingBoardBlock)) {
+            return Optional.empty();
+        }
+
+        CompoundTag nbt = context.targetInfo().nbt();
+        if (nbt != null && nbt.contains("CurrentCutStack")
+                && !ItemStack.parseOptional(context.contraptionEntity().level().registryAccess(),
+                nbt.getCompound("CurrentCutStack")).isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(ContraptionRemovalResult.single(
+                context.targetPos(), new ItemStack(state.getBlock().asItem())));
     }
 }
