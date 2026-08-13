24e5cd720e92f23a9d731456d739e53fb5fa1f71
笏天 <1404928002@qq.com>
2026-08-14 00:17:35 +0800

    feat: animate Tavern bar stools on contraptions

---
+++ src/main/java/com/sshakusora/kaleidoscope_contraption/content/behaviour/movement/TavernBarStoolMovementBehaviour.java
@@ -0,0 +1,26 @@
+package com.sshakusora.kaleidoscope_contraption.content.behaviour.movement;
+
+import com.simibubi.create.content.contraptions.behaviour.MovementContext;
+import com.simibubi.create.content.contraptions.render.ContraptionMatrices;
+import com.simibubi.create.foundation.virtualWorld.VirtualRenderWorld;
+import com.sshakusora.kaleidoscope_contraption.client.render.ContraptionBarStoolRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+
+/**
+ * Renders Tavern bar stool bodies as Create actors so they can follow seated passengers.
+ */
+public class TavernBarStoolMovementBehaviour extends TavernSeatMovementBehaviour {
+    @Override
+    public boolean disableBlockEntityRendering() {
+        return true;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void renderInContraption(MovementContext context, VirtualRenderWorld renderWorld,
+                                    ContraptionMatrices matrices, MultiBufferSource buffer) {
+        ContraptionBarStoolRenderer.render(context, renderWorld, matrices, buffer);
+    }
+}
